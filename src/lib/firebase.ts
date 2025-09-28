6f27e9970e076841e5c6134597a4d06f755b1991
Deepu V Nair <Deepu105045@gmail.com>
2025-09-28 09:39:12 +0000

    firebase integration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,11 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBNigP1oHgcc3CIO63KDMgaIC70nMdw8pc",
-  authDomain: "fam360-official.firebaseapp.com",
-  projectId: "fam360-official",
-  storageBucket: "fam360-official.appspot.com",
+  authDomain: "studio-5926457398-92ea0.firebaseapp.com",
+  projectId: "studio-5926457398-92ea0",
+  storageBucket: "studio-5926457398-92ea0.firebasestorage.app",
   messagingSenderId: "430792785599",
-  appId: "1:430792785599:web:28f9d1fd492029eefc900a",
-  measurementId: ""
+  appId: "1:430792785599:web:d901d11c64510e56fc900a"
 };
 
 // Initialize Firebase
